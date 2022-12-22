f2e6fde7fcfd95d628827967f21f06a6913e6def
areeb <areeb.malik@cooperativecomputing.com>
2022-12-22 12:30:26 +0500

    added few routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link, NavLink, } from 'react-router-dom';
-import Sidebar from './pages/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Products';
+import AddProduct from './pages/AddProduct';
 
 function App() {
   return (
@@ -9,15 +11,50 @@ function App() {
 
         <header>
           <nav>
-            {/* <Sidebar /> */}
+            <div className="mainContainer">
+              <div className="sideBar">
+                <nav>
+                  <ul className='fs-5 mt-4'>
+                    <NavLink to="/" className="nav-link py-1"><i className="bi bi-speedometer me-2"></i>Dashboard</NavLink>
+                    <NavLink to="product" className="nav-link py-1" ><i className="bi bi-collection me-2"></i>Manage Products</NavLink>
+                    <NavLink className="nav-link py-1" ><i className="bi bi-rocket-takeoff me-2"></i>Manage Codes</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-search me-2"></i>Manage Code Search</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-archive me-2"></i>Disable Codes</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-tags me-2"></i>Manage Labels</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-briefcase me-2"></i>Manage Batches</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-people me-2"></i>Manage Users</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-list-ul me-2"></i>Manage Roles</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-diagram-3 me-2"></i>Manage Categories</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-person-vcard me-2"></i>Manage Beekeepers</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-map me-2"></i>Manage Regions</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-envelope-at me-2"></i>Manage Emails</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-envelope-x me-2"></i>Invalid Codes</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-file-earmark me-2"></i>Reports</NavLink>
+                    <NavLink className="nav-link py-1"><i className="bi bi-sliders me-2"></i>Manage Site Settings</NavLink>
+                    <NavLink className="nav-link py-1" ><i className="bi bi-door-open-fill me-2"></i>Logout</NavLink>
+                  </ul>
+                </nav>
+              </div>
+
+              <div className='ms-4 mainComponents mt-4'>
+                <div className="dashboard">
+                  <main>
+                    <Routes>
+                      <Route path='/' element={<Dashboard />} />
+                      
+                      {/* Product routes */}                      
+                      <Route path='product' element={<Products />} />
+                      <Route path='product/add' element={<AddProduct />} />
+
+                      
+                    </Routes>
+                  </main>
+                </div>
+              </div>
+
+            </div>
           </nav>
         </header>
-
-        <main>
-          <Routes>
-            <Route path='/' element={<Sidebar />} />
-          </Routes>
-        </main>
       </BrowserRouter>
     </>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import images_1 from '../images/1.jpg';
+
 
 export default function Products() {
     return (
         <>
-            <form>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1 className="display-5">Manage Products</h1>
+                        <p className="display-6">View Products</p>
+                        <table className="table table-bordered borderColor mt-4">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">SKU</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img src={images_1} className='table_imgs' alt="first_image" />
+                                    </td>
+                                    <td>Item 1</td>
+                                    <td>Category 1</td>
+                                    <td>SKU 1</td>
+                                    <td>
+                                        <NavLink to='edit' className="text-decoration-none">
+                                            <button className='btn btn-outline-success mx-1'><i className="bi bi-pencil-square"></i>edit</button>
+                                        </NavLink>
+                                        <NavLink to='clip' className="text-decoration-none">
+                                            <button className='btn btn-outline-warning mx-1'><i className="bi bi-paperclip"></i>clip</button>
+                                        </NavLink>
+                                        <NavLink to='archive' className="text-decoration-none">
+                                            <button className='btn btn-outline-danger mx-1'><i className="bi bi-trash-fill"></i>archive</button>
+                                        </NavLink>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                <div className='text-end mt-4'>
+                    <NavLink to='add' className="text-decoration-none">
+                        <button className='btn btn-large fs-4 btn-outline-dark addProduct'>
+                            Add Products
+                        </button>
+                    </NavLink>
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </>
     )
 }
